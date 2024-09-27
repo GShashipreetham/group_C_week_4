b06b16e21794fef150e2cfce55db5f46d3532652
Sandeep Nalumasu <sandeepnalumasu@loyalistcollege.com>
2024-09-27 15:57:44 -0400

    Update main.go

--- main.go
+++ main.go
@@ -11,4 +11,14 @@ func reverseNumber(n int) int{
     remainder := n%10
     reversed = reversed*10 + remainder
 
+
+func countCharacters(s string) map[rune]int {
+      countMap := make(map[rune]int)
+      for _, char := range s {
+        countMap[char]++
+      }
+      return countMap
+  }
+
+    
   }
