8e59f430ff756d845f1ee2c3b7b9cd52d7700d33
saitejadugyala468 <dugyalasaiteja468@gmail.com>
2024-09-27 16:15:42 -0400

    Added function to calculate square

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("Welcome to Group C's Week 4!")
-
-  }
+	fmt.Println("Welcome to Group C's Week 4 Project!")
+	result := calculateSquare(5)
+	fmt.Printf("The square of 5 is %d\n", result)
+}
