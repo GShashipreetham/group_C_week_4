bb72f7c1dbdb497667d324db46b6fb4e417dc01f
Hemanth Mamidi <hemanthreddi7578@gmail.com>
2024-09-27 15:46:49 -0400

    Update main.go

--- main.go
+++ main.go
@@ -5,4 +5,10 @@ import "fmt"
 func main() {
   fmt.Println("Welcome to Group C's Week 4!")
 
+func reverseNumber(n int) int{
+  reversed := 0
+  for n != 0{
+    remainder := n%10
+    reversed = reversed*10 + remainder
+
   }
