20b75a599b85aade3db01954b7b8c70dc8d0ffad
saitejadugyala468 <dugyalasaiteja468@gmail.com>
2024-09-27 21:39:38 -0400

    C

--- main.go
+++ main.go
@@ -3,36 +3,8 @@ package main
 import "fmt"
 
 func main() {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-  fmt.Println("Welcome to Group C's Week 4!")
-  result := factorial (9)
-  fmt.Println("factorial is:" , result)
-
-<<<<<<< HEAD
-  print(countCharacters("Sandeep Nalumasu"))
-
-
-
-
-    
-  }
-=======
-  }
-=======
->>>>>>> 5e5a653c1cee9f6cf02c439677ed22a1bae815ca
-	fmt.Println("Welcome to Group C's Week 4!")
-
-	arr := []int{11, 15, 19, 25, 30, 40}
-	fmt.Println("Unsorted array:", arr)
-
-	BubbleSort(arr)
-
-	fmt.Println("Sorted array: ", arr)
+	var s int
+	fmt.Print("Enter the number of rows for the butterfly pattern: ")
+	fmt.Scan(&s)
+	cloud2003(s)
 }
-<<<<<<< HEAD
-=======
->>>>>>> 249de538224efbc44ba207b19a382256de3f9197
->>>>>>> e2a8dddeb1083bb295d5ecde8c444e698af8f79d
->>>>>>> 5e5a653c1cee9f6cf02c439677ed22a1bae815ca
